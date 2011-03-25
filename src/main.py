c8baa9568910f5c40885336324e6ac515f22510d
Gary Turovsky <flyhighplato@gmail.com>
2011-03-25 12:47:49 -0500

    Forgot to add the actuall main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+
+import java
+import javax.swing as swing
+import java.awt as awt
+
+
+def appMain():
+    win = swing.JFrame("Jython", size=(200, 200),windowClosing=exit)
+    win.contentPane.layout = awt.FlowLayout(  )
+    
+    field = swing.JTextField(preferredSize=(200,20))
+    field.setText("Hello world!")
+    field.setEnabled(False);
+    
+    win.contentPane.add(field)
+    
+    win.pack()
+    win.show()
+
+if __name__ ==  "__main__":     
+    appMain()
+    print("Hello world")
